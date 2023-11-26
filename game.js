f4c5d472d70be080d87f42cd5f1562fc9fb401c0
s-biagini <109245563+stephy-b@users.noreply.github.com>
2023-11-26 15:01:50 -0800

    level, title++ w/ btn clicks, reordered code

--- game.js
+++ game.js
@@ -7,63 +7,18 @@ const WRONGSOUND = new Audio("sounds/wrong.mp3")
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let gameStarted = false;
+let level = 0;
 
 
-
-const playSound = (color) => {
-  switch (color) {
-    case "red":
-      REDSOUND.play();
-      break;
-    case "blue":
-      BLUESOUND.play();
-      break;
-    case "green":
-      GREENSOUND.play();
-      break;
-    case "yellow":
-      YELLOWSOUND.play();
-      break;
-    default:
-      // If no matching color, play the "wrong" sound
-      WRONGSOUND.play();
-      break;
-  }
-};
-
-
-const flashButton = (color) => {
-  // Use jQuery to select the button by ID and toggle a class for flashing
-  $("#" + color).addClass("flash");
-
-  // After a short delay, remove the flashing class to stop the animation
-  setTimeout(() => {
-    $("#" + color).removeClass("flash");
-  }, 400);
-  playSound(color); // Call playSound when flashing the button
-};
-
-
-const nextSequence = () => {
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-
-  // Call the flashButton function to make the chosen color flash
-  flashButton(randomChosenColor);
-
-  return randomChosenColor;
-};
-
-
-const animatePress = (currentColor) => {
-  $("#" + currentColor).addClass("pressed");
-
-  // After a short delay, remove the pressed class to revert the animation
-  setTimeout(() => {
-    $("#" + currentColor).removeClass("pressed");
-  }, 200);
-};
+$(document).keydown(function() {
+  if (!gameStarted) {
+    // Change title on game start
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    gameStarted = true;
+  }  
+});  
 
 
 $(".btn").on("click", (event) => {
@@ -74,6 +29,52 @@ $(".btn").on("click", (event) => {
   playSound(userChosenColor);
   // Call animation
   animatePress(userChosenColor);
-  // Log the pattern to the console
-  console.log(userClickedPattern);
-});  
+
+  setTimeout(function () {
+    nextSequence();
+  }, 1000);
+});
+
+
+const flashButton = (color) => {
+  // Use jQuery to select the button by ID and toggle a class for flashing
+  $("#" + color).addClass("flash");
+
+  // After a short delay, remove the flashing class to stop the animation
+  setTimeout(() => {
+    $("#" + color).removeClass("flash");
+  }, 400);    
+  playSound(color); // Call playSound when flashing the button
+};    
+
+
+const animatePress = (currentColor) => {
+  $("#" + currentColor).addClass("pressed");
+
+  // After a short delay, remove the pressed class to revert the animation
+  setTimeout(() => {
+    $("#" + currentColor).removeClass("pressed");
+  }, 200);  
+};  
+
+
+const nextSequence = () => {
+  // Increment the level
+  level++;
+  // Update the displayed level
+  $("#level-title").text("Level " + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  // Call the flashButton function to make the chosen color flash
+  flashButton(randomChosenColor);
+  return randomChosenColor;
+};    
+
+
+const playSound = (color) => {
+  let audio = new Audio("sounds/" + color + ".mp3");
+  audio.play();
+  };    
